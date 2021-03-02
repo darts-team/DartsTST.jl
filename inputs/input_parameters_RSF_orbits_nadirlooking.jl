c58367ab0c4b9c4601cc77848e586fa415e74470
ilgin seker <ilgin.seker@jpl.nasa.gov>
2021-03-02 13:41:40 -0800

    improved performance metrics module structure, added PSLR as a metric

--- inputs/input_parameters_RSF_orbits_nadirlooking.jl
+++ inputs/input_parameters_RSF_orbits_nadirlooking.jl
@@ -7,22 +7,22 @@ mode=1 #1: SAR (ping-pong), 2:SIMO, 3:MIMO
 tx_el=1 # which element transmits for SIMO (max value N)
 # radar parameters
 fc=1e9 # center frequency (Hz)
-fp=2 # pulse repetition frequency (Hz)
-SNR=30 # SNR for single platform and single pulse before fast-time processing dB (for additive random noise only) TODO calculate based on sigma-zero (which depends on target type, wavelength, look angle, polarization) and NESZ (which depends on radar specs and processing)
+fp=3 # pulse repetition frequency (Hz)
+SNR=50 # SNR for single platform and single pulse before fast-time processing dB (for additive random noise only) TODO calculate based on sigma-zero (which depends on target type, wavelength, look angle, polarization) and NESZ (which depends on radar specs and processing)
 # platform locations in xyz taken from orbits (including slow-time)
 orbit_filename="orbitOutput_082020.nc" # position in km, time in sec
-SAR_duration=2 # synthetic aperture duration (s)
+SAR_duration=3 # synthetic aperture duration (s)
 SAR_start_time=0 # SAR imaging start time (s)
 # target locations (volumetric grid) defined in geo (θϕh)
 t_θ=0 # deg latitude
 t_ϕ=0 # deg longitude
 t_h=0 # m  heights
 # image/scene pixel coordinates
-s_θ=-0.00015:0.000005:0.00015 # deg latitude
-s_ϕ=-0.0015:0.00005:0.0015 # deg longitude
-s_h=0#-300:20:300 # m  heights
+s_θ=-0.0001:0.000001:0.0001 # deg latitude
+s_ϕ=-0.0005:0.00001:0.0005 # deg longitude
+s_h=-40:1:40 # m  heights
 # range spread function (RSF) parameters
-enable_fast_time=false # whether to enable or disable fast-time axis, 0:disable, 1: enable
+enable_fast_time=true # whether to enable or disable fast-time axis, 0:disable, 1: enable
 enable_thermal_noise=false # whether to enable or disable random additive noise (e.g. thermal noise)
 Trx=300e-6 # s duration of RX window (may need to be increased if aperture or scene is large) TODO (adjust based on max/min range)
 pulse_length=10e-6 # s pulse length
